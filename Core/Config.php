3f284c35dbddee6da07bbff1f556147f788fed1d
Roxane Riff <roxanerg@gmail.com>
2019-03-05 17:50:22 +0100

    Création de classe Config

---
+++ Core/Config.php
@@ -0,0 +1,43 @@
+<?php
+namespace Core;
+/**
+ * Application configuration
+ *
+ * PHP version 7.0
+ */  
+class Config
+{
+     private $settings = array();
+     private static $instance =  null;
+  
+     public function __construct()
+     {
+         include_once('../App/Config.php');
+         $this->settings = $config;
+     }
+    
+     public static function get()
+     {
+         if (is_null(self::$instance)) 
+         {
+            self::$instance = new Config();
+         }
+         return self::$instance;
+     }
+     
+     public function db($key)
+     {
+         if (isset($this->settings['database'][$key])) {
+             return $this->settings['database'][$key];
+         }
+         return null;
+     }
+     
+     public function config($key)
+     {
+         if (isset($this->settings['config'][$key])) {
+             return $this->settings['config'][$key];
+         }
+         return null;
+     }
+ }
