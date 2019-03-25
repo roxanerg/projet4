9706e59ccda8c31bb8ab616fc7f31c732e24e2e0
roxanerg <roxanerg@gmail.com>
2019-03-25 17:35:06 +0100

    Correction du bug pour désignaler un commentaire

--- App/View/Admin/index.php
+++ App/View/Admin/index.php
@@ -1,11 +1,73 @@
-<h2 id="welcome">Bienvenue</h2>
+<h2 id="welcome">Bienvenue !</h2>
 
-<h3 id="flagged">Commentaires signalés :</h3>
+<table class="display table table-bordered table-hover" id="comments">
 
-<?php foreach ($vars['moderateComm'] as $comms): ?>
+	<div id="notice"><a href="/docs/docu.php" class="get_doc btn btn-primary">Téléchargez la notice d'utilisation
+	<i class="download fas fa-file-download"></i></a></div><br/>
 
-    <h4><?= $comms['auteur']?></h4>
+	<h3 id="flagged" class="badge badge-light">Commentaires signalés :</h3>
+        <caption>Commentaires signalés</caption>
 
-    <p><?= $comms['commentaire']?></p>
+        <thead class="thead-light">
+            <tr>
+                <th scope="col">Auteur</th>
+                <th scope="col">Commentaire</th>
+                <th scope="col">Publié le</th>
+				<th scope="col">Supprimer</th>
+				<th scope="col">Ne plus signaler</th>
 
-<?php endforeach; ?>
+            </tr>
+        </thead>
+		
+        <tbody>
+
+        <?php foreach ($vars['moderateComm'] as $comments): ?>
+            <tr>
+                <td><?=$comments['auteur']?></td>
+                <td><?=$comments['commentaire']?></td>
+                <td><?=$comments['date_commentaire']?></td>
+                <td>
+                <a href="?action=deleteComment&id=<?= $comments['id']?>" scope="col" class="supp btn btn-outline-danger"><i class="fas fa-times"></i></a>
+                </td>
+				<td>
+                <button name="<?= $comments['id']?>" scope="col" class="unreport btn btn-warning"><i class="fas fa-undo-alt"></i></button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+
+        </tbody>
+    </table>
+
+	<script>
+		$(document).ready(function() {
+
+			$('.unreport').on('click', function(event) {
+
+            var comment_id = $(this).attr('name');
+
+            $.ajax ({
+                url:"index.php?action=unreportComment&id=" + comment_id,
+                method:"POST",
+				context: this,
+				data: { id: comment_id, action: 'unreportComment' },
+                dataType:"text",
+                success:function(data)
+                {
+                    alert('Le commentaire n\'est plus signalé'+ comment_id + '-'+ data);
+					$(this).parent().parent().hide();
+                }
+			})
+        });
+
+			$(".supp").click(function() {
+			var confirmed = confirm("Êtes-vous sur de vouloir supprimer le commentaire ?");  
+
+			if (confirmed) {
+				return true;
+			} else { 
+				return false;
+			}
+		});
+	});
+
+	</script>
