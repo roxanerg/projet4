e17ee7a5fa68764b20f73faa46d62e248e5db9f3
roxanerg <roxanerg@gmail.com>
2019-03-25 18:11:26 +0100

    Ajout de la fonction edit()

--- App/Controller/Admin/Users.php
+++ App/Controller/Admin/Users.php
@@ -5,21 +5,85 @@
 
 class Users extends \Core\Controller
 {  
-    function viewAll()
+    /**
+     * @fn	public function viewAll()
+     *
+     * @brief	View all users
+     *
+     * @author	Roxane Riff
+     * @date	25/03/2019
+     *
+     * @returns	A function.
+     */
+
+    public function viewAll()
     {
         $usersModel = new \App\Model\Users();
         $users = $usersModel->list();
         $this->view->displayAdmin('users', ['users' => $users]);
     }
 
-    function add($post)
+    /**
+     * @fn	public function add($post)
+     *
+     * @brief	Adds user
+     *
+     * @author	Roxane Riff
+     * @date	25/03/2019
+     *
+     * @param	post	The user infos to add.
+     *
+     * @returns	A function.
+     */
+
+    public function add($post)
     {
         $userModel = new \App\Model\Users();
-        $add_user = $userModel->add($post['name'], $post['mail'], $post['password']);
+        $add_user = $userModel->add($post['mail'], $post['password'], $post['name']);
         return $add_user;
     }
 
-    function delete($id=0)
+	/**
+	 * @fn	public function edit($id=0, $post)
+	 *
+	 * @brief	Edits the user
+	 *
+	 * @author	Roxane Riff
+	 * @date	25/03/2019
+	 *
+	 * @param	optional $id	The optional user $id.
+	 * @param	post			The user infos.
+	 *
+	 * @returns	A function.
+	 */
+
+	public function edit($id=0, $post)  
+    {      
+        $userModel = new \App\Model\Users();
+        if (!empty($post)) {
+            $userModel->update($id, $post['name'], $post['mail']);
+        }
+		if (!empty($post['password'])) {
+			$userModel->update_passw($id, $post['password']);
+		} 
+        $user = $userModel->get($id);
+        $this->view->displayAdmin('user', ['user' => $user]);
+     }
+
+    /**
+     * @fn	public function delete($id=0)
+     *
+     * @brief	Deletes the given user
+     *
+     * @author	Roxane Riff
+     * @date	25/03/2019
+     *
+     * @param	optional $id	The given user $id to delete.
+     *
+     * @returns	A function.
+     */
+
+    public function delete($id=0)
     {
         $userModel = new \App\Model\Users();
         $deleted = $userModel->delete($id);
