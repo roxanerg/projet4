5a9da4894312415dd4fcfda7e12629b8a9635267
roxanerg <roxanerg@gmail.com>
2019-03-25 17:46:13 +0100

    Ajout du plugin DataTable Réglage des options de TinyMCE Ajout d'un menu "hamburger" pour le responsive

--- App/View/Admin/header.php
+++ App/View/Admin/header.php
@@ -6,38 +6,73 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" href="./www/css/admin.css" />
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
+	<link rel="stylesheet" href="/css/admin.css" />
+ 
 
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
     <script src="./www/tinymce/tinymce.min.js"></script> 
     <script>
         tinymce.init({
+			setup: function (editor) {
+				editor.on('change', function () {
+					editor.save();
+				});
+			},
             selector :'.txt_episode',
             content_css : 'www/css/admin.css',
-            contextmenu : "link image imagetools table spellchecker",
+			menubar: "edit format",
+            contextmenu : "link table spellchecker",
             language : 'fr_FR',
             cleanup : true
             });
     </script>
 </head>
 
-<body>
-    <div class="wrapper">
-        <header id="header_admin">
+<body id="body">
+    <header id="header_admin">
 
-            <div id="logo_box">
-                <a href="?action=indexAdmin"><img src="www/images/logo3.png" id="logo" alt="logo Jean Forteroche"></a>
-            </div>
+        <div id="logo_box">
+            <a href="?action=indexAdmin"><img src="www/images/Logo3.png" id="logo" alt="logo Jean Forteroche"></a>
+        </div>
 
-            <nav id="navbar">
-                <ul>
-                    <li><a href="?action=indexAdmin"><i class="fas fa-solar-panel"></i>Tableau de bord</a></li>
-                    <li><a href="?action=allEpisodes"><i class="fas fa-pencil-alt"></i>Episodes</a></li>
-                    <li><a href="?action=allComments"><i class="far fa-comment-alt"></i>Commentaires</a></li>
-                    <li><a href="?action=editBio"><i class="fas fa-user-edit"></i>Biographie</a></li>
-                    <li><a href="?action=allUsers"><i class="fas fa-users"></i>Utilisateurs</a></li>  
-                    <li><a href="?action=logoutAdmin"><i class="fas fa-power-off"></i>LogOut</a></li> 
-                </ul>
-            </nav>
-        </header>
+        <nav id="navbar">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="?action=indexAdmin" class="nav-link active"><i class="fas fa-solar-panel"></i>Tableau de bord</a></li>
+                <li class="nav-item"><a href="?action=allEpisodes" class="nav-link"><i class="fas fa-pencil-alt"></i>Episodes</a></li>
+                <li class="nav-item"><a href="?action=allComments" class="nav-link"><i class="far fa-comment-alt"></i>Commentaires</a></li>
+                <li class="nav-item"><a href="?action=editBio" class="nav-link"><i class="fas fa-user-edit"></i>Biographie</a></li>
+                <li class="nav-item"><a href="?action=allUsers" class="nav-link"><i class="fas fa-users"></i>Utilisateurs</a></li>  
+                <li class="nav-item" id="logout"><a href="?action=logoutAdmin" class="nav-link"><i class="fas fa-power-off"></i>LogOut</a></li> 
+            </ul>
+        </nav>
+		
+    </header>
 
-        <div class="container-fluid">
+    <header id="header_sm">
+
+        <div id="logo_box">
+            <a href="?action=indexAdmin"><img src="www/images/Logo3.png" id="logo" alt="logo Jean Forteroche"></a>
+        </div>
+		
+		<nav id="hamburger">
+			<div id="toggle">
+				<input type="checkbox" />
+				<span></span>
+				<span></span>
+				<span></span>
+
+				<ul id="hamb_nav">
+					<li class="nav-item"><a href="?action=indexAdmin" class="nav-link active"><i class="fas fa-solar-panel"></i>Tableau de bord</a></li>
+					<li class="nav-item"><a href="?action=allEpisodes" class="nav-link"><i class="fas fa-pencil-alt"></i>Episodes</a></li>
+					<li class="nav-item"><a href="?action=allComments" class="nav-link"><i class="far fa-comment-alt"></i>Commentaires</a></li>
+					<li class="nav-item"><a href="?action=editBio" class="nav-link"><i class="fas fa-user-edit"></i>Biographie</a></li>
+					<li class="nav-item"><a href="?action=allUsers" class="nav-link"><i class="fas fa-users"></i>Utilisateurs</a></li>  
+					<li class="nav-item" id="logout"><a href="?action=logoutAdmin" class="nav-link"><i class="fas fa-power-off"></i>LogOut</a></li> 
+				</ul>
+			</div>
+		</nav>
+    </header>
+
+<div class="wrapper">
